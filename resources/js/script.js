87be54d6a701aa8bb472399d0ce5eaf2b4dbceba
williamhoman <williamfhoman@gmail.com>
2016-10-03 20:50:49 -0500

    version-one
    Version 1

---
+++ resources/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+  // var waypoints = $('#options-only').waypoint({
+  //   handler: function(direction) {
+  //     notify(this.element.id + ' hit')
+  //   }
+  // })
+
+  $('.js--section-me').waypoint(function(direction) {
+      if (direction == "down") {
+        $('nav').addClass('sticky');
+      } else {
+        $('nav').removeClass('sticky');
+      }
+  });
+
+
+
+
+
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+});
